3e01faf8dd8964fc3b57f6b60ad40988b87db019
rvinder singh <ravinderfb21@gmail.com>
2019-04-04 02:12:10 -0400

    commited

--- assignmnt3/Program.cs
+++ assignmnt3/Program.cs
@@ -117,4 +117,51 @@ class DataStructureStack : IDataStructure
         Sort(Stack);
 
     }
+    public void Run()
+    {
+
+      for (int i = 0; i < 20; i++)
+
+        {
+            s1.Push(new Student());
+        }
+        Sort1 = new Sort_DataStructure(Sorter);
+
+    }
+    public void Sort() { }
+
+}
+
+}
+namespace BubbleSort
+{
+    class MySort
+    {
+        static void Main2(string[] args)
+
+        {
+            int[] arr = { 78, 55, 45, 98, 13 };
+           int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+            
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                   }
+
+                }
+            }
+            Console.WriteLine("Sorted:");
+            foreach (int p in arr)
+                Console.Write(p + " ");
+            Console.Read();
+        }
+    }
+
+}
 }
